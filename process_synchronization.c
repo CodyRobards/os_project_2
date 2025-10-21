9cef88087f6300b4436456f7873387ea507a0d22
CodyRobards <cody.robards.95@gmail.com>
2025-10-21 17:52:54 -0500

    Update thread function signatures and calls

--- process_synchronization.c
+++ process_synchronization.c
@@ -9,32 +9,35 @@
 
 int i;
 sem_t s1, s2;
-void *Thread1(){
-sem_wait(&s1);
-printf("I am first thread\n");
-for (i=10;i<20;i++){
-printf("%d\n",i);
-}
-sem_post(&s2);
-return NULL;
-}
-
-void *Thread2(){
-printf("I am second thread\n");
-for (i=0;i<10;i++){
-printf("%d\n",i);
-}
-sem_post(&s1);
-return NULL;
+void *Thread1(void *arg){
+    (void)arg;
+    sem_wait(&s1);
+    printf("I am first thread\n");
+    for (i=10;i<20;i++){
+        printf("%d\n",i);
+    }
+    sem_post(&s2);
+    return NULL;
 }
 
-void *Thread3(){
-sem_wait(&s2);
-printf("I am third thread\n");
-for(i=20;i<31;i++){
-printf("%d\n",i);
+void *Thread2(void *arg){
+    (void)arg;
+    printf("I am second thread\n");
+    for (i=0;i<10;i++){
+        printf("%d\n",i);
+    }
+    sem_post(&s1);
+    return NULL;
 }
-return NULL;
+
+void *Thread3(void *arg){
+    (void)arg;
+    sem_wait(&s2);
+    printf("I am third thread\n");
+    for(i=20;i<31;i++){
+        printf("%d\n",i);
+    }
+    return NULL;
 }
 
 int main()
@@ -44,9 +47,9 @@ pthread_t tid,tid2,tid3;
 sem_init(&s1,0,0);
 sem_init(&s2,0,0);
 
-pthread_create(&tid,NULL,Thread1,(void *)&tid);
-pthread_create(&tid2,NULL,Thread2,(void *)&tid2);
-pthread_create(&tid3,NULL,Thread3,(void *)&tid3);
+pthread_create(&tid,NULL,Thread1,NULL);
+pthread_create(&tid2,NULL,Thread2,NULL);
+pthread_create(&tid3,NULL,Thread3,NULL);
 pthread_join(tid,NULL);
 pthread_join(tid2,NULL);
 pthread_join(tid3,NULL);
